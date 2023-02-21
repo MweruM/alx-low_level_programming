01de56c5936727dc6584f53d6ae0a008bae91d33
MweruM <nyamburam095@gmail.com>
2023-02-21 01:32:35 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _islower(int c);
 
 int _isalpha(int c);
 
-int print_sign(intn);
+int print_sign(int n);
 
 int _abs(int);
 
@@ -18,7 +18,7 @@ void jack_bauer(void);
 
 void times_table(void);
 
-int add(int,int);
+int add(int, int);
 
 void print_to_98(int n);
 
